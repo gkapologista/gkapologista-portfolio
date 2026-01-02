fbe79534c8523434d5c658ee2aea7b2784e70ebb
Geoff Kevin G. Apologista <gkapologista0800@gmail.com>
2026-01-03 04:32:13 +0800

    Add GitHub Pages deployment workflow and configure publicPath

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/gkapologista-portfolio/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
